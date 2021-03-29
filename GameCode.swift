7ec05013c392f92d6beb5c7b0b4e999d06657772
reshetnikd <dmitry.reshetnik@icloud.com>
2021-03-29 13:51:38 +0300

    Add challenge

--- GameCode.swift
+++ GameCode.swift
@@ -2,18 +2,6 @@ import Foundation
 
 let ball = OvalShape(width: 40, height: 40)
 
-let barrierWidth = 300.0
-let barrierHeight = 25.0
-
-let barrierPoints = [
-    Point(x: 0, y: 0),
-    Point(x: 0, y: barrierHeight),
-    Point(x: barrierWidth, y: barrierHeight),
-    Point(x: barrierWidth, y: 0)
-]
-
-let barrier = PolygonShape(points: barrierPoints)
-
 let funnelPoints = [
     Point(x: 0, y: 50),
     Point(x: 80, y: 50),
@@ -23,14 +11,8 @@ let funnelPoints = [
 
 let funnel = PolygonShape(points: funnelPoints)
 
-let targetPoints = [
-    Point(x: 10, y: 0),
-    Point(x: 0, y: 10),
-    Point(x: 10, y: 20),
-    Point(x: 20, y: 10)
-]
-
-let target = PolygonShape(points: targetPoints)
+var barriers: [Shape] = []
+var targets: [Shape] = []
 
 /*
 The setup() function is called once when the app launches. Without it, your app won't compile.
@@ -47,6 +29,7 @@ fileprivate func setupBall() {
     ball.hasPhysics = true
     ball.isDraggable = false
     ball.fillColor = .blue
+    ball.bounciness = 0.6
     ball.onCollision = ballCollided(with:)
     ball.onExitedScene = ballExitedScene
     ball.onTapped = resetGame
@@ -54,8 +37,20 @@ fileprivate func setupBall() {
     scene.trackShape(ball)
 }
 
-fileprivate func setupBarrier() {
-    barrier.position = Point(x: 200, y: 150)
+fileprivate func addBarrier(at position: Point, width: Double, height: Double, angle: Double) {
+    let barrierPoints = [
+        Point(x: 0, y: 0),
+        Point(x: 0, y: height),
+        Point(x: width, y: height),
+        Point(x: width, y: 0)
+    ]
+    
+    let barrier = PolygonShape(points: barrierPoints)
+    
+    barriers.append(barrier)
+    
+    barrier.position = position
+    barrier.angle = angle
     barrier.hasPhysics = true
     barrier.isImmobile = true
     barrier.fillColor = .brown
@@ -70,8 +65,19 @@ fileprivate func setupFunnel() {
     scene.add(funnel)
 }
 
-fileprivate func setupTarget() {
-    target.position = Point(x: 200, y: 400)
+fileprivate func addTarget(at position: Point) {
+    let targetPoints = [
+        Point(x: 10, y: 0),
+        Point(x: 0, y: 10),
+        Point(x: 10, y: 20),
+        Point(x: 20, y: 10)
+    ]
+    
+    let target = PolygonShape(points: targetPoints)
+    
+    targets.append(target)
+    
+    target.position = position
     target.hasPhysics = true
     target.isImmobile = true
     target.isImpermeable = true
@@ -85,22 +91,33 @@ func setup() {
     setupBall()
     
     // Add a barrier to the scene.
-    setupBarrier()
+    addBarrier(at: Point(x: 200, y: 150), width: 80, height: 25, angle: 0.1)
+    addBarrier(at: Point(x: 100, y: 150), width: 30, height: 15, angle: -0.2)
+    addBarrier(at: Point(x: 300, y: 150), width: 100, height: 25, angle: 0.3)
+    
+    // Add a target to the scene.
+    addTarget(at: Point(x: 133, y: 614))
+    addTarget(at: Point(x: 111, y: 474))
+    addTarget(at: Point(x: 256, y: 280))
+    addTarget(at: Point(x: 151, y: 242))
+    addTarget(at: Point(x: 165, y: 40))
     
     // Add a funnel to the scene.
     setupFunnel()
     
-    // Add a target to the scene.
-    setupTarget()
-    
     resetGame()
+    
+    scene.onShapeMoved = printPosition(of:)
 }
 
 // Drops the ball by moving it to the funnel's position.
 func dropBall() {
     ball.position = funnel.position
     ball.stopAllMotion()
-    barrier.isDraggable = false
+    
+    for barrier in barriers {
+        barrier.isDraggable = false
+    }
 }
 
 // Handles collisions between the ball and the targets.
@@ -111,10 +128,16 @@ func ballCollided(with otherShape: Shape) {
 }
 
 func ballExitedScene() {
-    barrier.isDraggable = true
+    for barrier in barriers {
+        barrier.isDraggable = true
+    }
 }
 
 // Resets the game by moving the ball below the scene, which will unlock the barriers.
 func resetGame() {
     ball.position = Point(x: 0, y: -80)
+}
+
+func printPosition(of shape: Shape) {
+    print(shape.position)
 }
